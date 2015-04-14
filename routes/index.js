07287b549dc493277533272ec2bda538e8745896
yuanyuan <yuanyuan07@baidu.com>
2015-04-14 22:32:01 +0800

    modified:   routes/index.js 	modified:   views/index.jade 	modified:   views/layout.jade
     Untracked files:
    	views/index.html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ô¬ÊÏ×åÆ×' });
+  res.render('index', { title: 'è¢æ°æ—è°±' });
 });
 
 module.exports = router;
